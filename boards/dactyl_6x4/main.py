5e3fe5d5e1c8426a94986e538dacb8d5a86bdab0
pullenrc <ryan.pullen@live.com>
2021-07-30 13:40:21 -0500

    added oled extension

---
+++ boards/dactyl_6x4/main.py
@@ -0,0 +1,217 @@
+import board
+
+from kb_rp2040 import KMKKeyboard
+
+from kmk.handlers.sequences import send_string, simple_key_sequence
+from kmk.keys import KC
+from kmk.extensions.oled import OLED
+from kmk.modules.encoder import EncoderHandler
+from kmk.modules.layers import Layers
+from kmk.modules.split import Split, SplitSide, SplitType
+
+
+keyboard = KMKKeyboard()
+
+# custom keys used for encoder actions
+Zoom_in = KC.LCTRL(KC.EQUAL)
+Zoom_out = KC.LCTRL(KC.MINUS)
+
+split = Split()
+oled = OLED([128],[32],[board.SCL],[board.SDA])
+layers_ext = Layers()
+
+# Cleaner key names
+_______ = KC.TRNS
+XXXXXXX = KC.NO
+
+#  for use in the encoder extension
+encoder_map = [
+    [
+        (
+            KC.VOLU,
+            KC.VOLD,
+            2,
+        ),  #  Only 1 encoder is being used, so only one tuple per layer is required
+    ],
+    [
+        (Zoom_in, Zoom_out, 1),
+    ],
+    [
+        (_______, _______, 1),  #  no action taken by the encoder on this layer
+    ],
+]
+
+encoder_ext = EncoderHandler([board.TX], [board.D4], encoder_map)
+encoder_ext.encoders[0].is_inverted = True
+keyboard.modules = [layers_ext, split, encoder_ext]
+keyboard.extensions = [oled]
+#
+# custom keys
+NEW = KC.LCTL(KC.N)
+NEW_DIR = KC.LCTL(KC.LSFT(KC.N))
+CAD = KC.LCTL(KC.LALT(KC.DEL))
+RES = KC.LCTL(KC.LSFT(KC.ESC))
+FE = KC.LGUI(KC.E)
+LT1_DEL = KC.LT(1, KC.DEL)
+LT2_ENT = KC.LT(2, KC.ENT)
+SAVE_AS = KC.LCTL(KC.LSFT(KC.S))
+PSCR = KC.LGUI(KC.PSCR)
+SNIP = simple_key_sequence(
+    (
+        KC.LGUI,
+        KC.MACRO_SLEEP_MS(25),
+        KC.S,
+        KC.N,
+        KC.I,
+        KC.P,
+        KC.MACRO_SLEEP_MS(25),
+        KC.ENT,
+    )
+)
+
+# programming layer keys
+UINT = simple_key_sequence(
+    (
+        KC.U,
+        KC.I,
+        KC.N,
+        KC.T,
+    )
+)
+INT = simple_key_sequence(
+    (
+        KC.I,
+        KC.N,
+        KC.T,
+    )
+)
+DOUBLE = simple_key_sequence(
+    (
+        KC.D,
+        KC.O,
+        KC.U,
+        KC.B,
+        KC.L,
+        KC.E,
+    )
+)
+BOOL = simple_key_sequence(
+    (
+        KC.B,
+        KC.O,
+        KC.O,
+        KC.L,
+    )
+)
+BYTE = simple_key_sequence(
+    (
+        KC.B,
+        KC.Y,
+        KC.T,
+        KC.E,
+    )
+)
+SBYTE = simple_key_sequence(
+    (
+        KC.S,
+        KC.B,
+        KC.Y,
+        KC.T,
+        KC.E,
+    )
+)
+CHAR = simple_key_sequence(
+    (
+        KC.C,
+        KC.H,
+        KC.A,
+        KC.R,
+    )
+)
+GETSET = simple_key_sequence(
+    (
+        KC.LBRC,
+        KC.SPC,
+        KC.G,
+        KC.E,
+        KC.T,
+        KC.SCLN,
+        KC.SPC,
+        KC.S,
+        KC.E,
+        KC.T,
+        KC.SCLN,
+        KC.SPC,
+        KC.RBRC,
+    )
+)
+PUBLIC = simple_key_sequence(
+    (
+        KC.P,
+        KC.U,
+        KC.B,
+        KC.L,
+        KC.I,
+        KC.C,
+    )
+)
+DEBUGWL = simple_key_sequence(
+    (
+        KC.LSFT(KC.D),
+        KC.E,
+        KC.B,
+        KC.U,
+        KC.G,
+        KC.DOT,
+        KC.LSFT(KC.W),
+        KC.R,
+        KC.I,
+        KC.T,
+        KC.E,
+        KC.LSFT(KC.L),
+        KC.I,
+        KC.N,
+        KC.E,
+        KC.LSFT(KC.N9),
+    )
+)
+PRINT = simple_key_sequence(
+    (
+        KC.P,
+        KC.R,
+        KC.I,
+        KC.N,
+        KC.T,
+    )
+)
+
+
+keyboard.keymap = [
+    [  #QWERTY
+        KC.CAPS,   KC.Q,      KC.W,      KC.E,      KC.R,        KC.T,           KC.Y,      KC.U,      KC.I,      KC.O,      KC.P,       KC.PSLS,
+        KC.TAB,    KC.A,      KC.S,      KC.D,      KC.F,        KC.G,           KC.H,      KC.J,      KC.K,      KC.L,      KC.SCLN,    KC.QUOT,
+        KC.ESC,    KC.Z,      KC.X,      KC.C,      KC.V,        KC.B,           KC.N,      KC.M,      KC.COMM,   KC.DOT,    KC.SLSH,    FE,
+        KC.LALT,   KC.LSFT,   KC.LCTL,   KC.BSPC,   KC.MO(1),    KC.MO(3),      KC.MO(3),   KC.MO(2),  KC.SPC,    KC.ENT,    KC.RSFT,    KC.RGUI,
+    ],
+    [  #LOWER
+        KC.BSLS,   KC.NO,     KC.HOME,     KC.UP,      KC.END,    NEW_DIR,            KC.N5,      KC.N6,      KC.N7,       KC.N8,      KC.N9,     KC.BSLS,
+        KC.F2,     KC.NO,     KC.LEFT,     KC.DOWN,    KC.RGHT,   KC.HASH,        KC.N0,      KC.N1,      KC.N2,       KC.N3,      KC.N4,     KC.QUOT,
+        KC.LSFT,   KC.NO,     KC.NO,       KC.NO,      KC.TAB,    KC.UNDS,        KC.MINS,    KC.PPLS,    KC.MINS,     KC.PAST,    KC.PSLS,   KC.LBRC,
+        _______,   _______,   _______,     _______,    _______,   _______,        _______,    _______,    _______,     KC.EQUAL,    _______,    _______,
+    ],
+    [  #RAISE
+        KC.BSLS,   KC.NO,      KC.NO,      KC.LCBR,   KC.RCBR,    KC.AT,          INT,        GETSET,     KC.UP,     KC.NO,     KC.NO,     KC.NO,
+        KC.TAB,    KC.NO,      KC.NO,      KC.LPRN,   KC.RPRN,    KC.DLR,         BOOL,       KC.LEFT,    KC.DOWN,   KC.RGHT,   KC.NO,     KC.NO,
+        KC.LSFT,   KC.NO,      KC.NO,      KC.LBRC,   KC.RBRC,    KC.PERC,        UINT,       DOUBLE,     KC.NO,     KC.NO,     KC.NO,     KC.NO,
+        _______,   _______,    _______,    _______,   _______,    _______,        _______,    _______,    _______,   _______,   _______,   _______,
+    ],
+    [  #ADJUST
+        KC.BSLS,   KC.NO,      KC.NO,      KC.LCBR,    KC.RCBR,    KC.AT,          INT,        GETSET,     KC.UP,     KC.NO,      KC.NO,     KC.NO,
+        KC.TAB,    KC.NO,      KC.NO,      KC.LPRN,    KC.RPRN,    KC.DLR,         BOOL,       KC.LEFT,    KC.DOWN,   KC.RGHT,    KC.NO,     KC.NO,
+        KC.LSFT,   KC.NO,      KC.NO,      KC.LBRC,    KC.RBRC,    KC.PERC,        UINT,       DOUBLE,     KC.NO,     KC.NO,      KC.NO,     KC.NO,
+        _______,   _______,    _______,    _______,    _______,    _______,        _______,    _______,    _______,   _______,    _______,   _______,
+    ]
+]
+
+if __name__ == '__main__':
+    keyboard.go()
---
+++ kmk/extensions/oled.py
@@ -0,0 +1,166 @@
+import adafruit_ssd1306
+import board
+import busio
+
+from kmk.extensions import Extension
+from kmk.graphics import Graphics as Icon
+
+
+class OLED(Extension):
+    def __init__(self,width,height,scl,sda):
+        self.enabled = True
+        self.i2c_bus0 = None
+        self.i2c_bus1 = None
+        self.oleds = []
+        self.oled_sda = sda
+        self.oled_scl = scl
+        self.oled_height = height
+        self.oled_width = width
+        self.oled_bus = None
+        self.oled_count = len(self.oled_width)
+        self.oled_addr = [0x31, 0x32]
+        self.graphics = None
+        self.current_key = ''
+        self.prev_key = ''
+        self.keylog_update = False
+        self.prev_key = ''
+        self.prev_layer_name = 'BASE'
+        self.layer_names = []
+        self.key_log = []
+        self.prev_key_string = ''
+        self.prev_key_log = []
+        # mode badge handling
+        self.prev_active_layer = []
+        self.mode_badge = []
+        self.prev_mode_badge = []
+        self.prev_logo = []
+        self.icon = Icon()
+        self.init()
+
+    def init(self):
+        #self.i2c_bus0 = busio.I2C(self.oled_scl[0], self.oled_sda[0])
+        #self.i2c_bus0 = busio.I2C(board.SCL, board.SDA)
+        for i in range(self.oled_count):
+            self.oleds.append(
+                adafruit_ssd1306.SSD1306_I2C(
+                    #self.oled_width[i], self.oled_height[i], self.i2c_bus0
+                    128, 32, busio.I2C(board.SCL, board.SDA)
+                )
+            )
+        self.draw_logo(self.oleds[0], self.icon.logos["Warhorse"])
+
+    def __repr__(self):
+        return #'LED({})'.format(self._to_dict())
+
+    def _to_dict(self):
+        return
+
+
+    def on_runtime_enable(self, sandbox):
+        return
+
+    def on_runtime_disable(self, sandbox):
+        return
+
+    def during_bootup(self, sandbox):
+
+        return
+
+    def before_matrix_scan(self, sandbox):
+        #self.draw_logo(self.oleds[0], self.icon.logos["Warhorse"])
+        return
+
+    def after_matrix_scan(self, sandbox):
+        return
+
+    def before_hid_send(self, sandbox):
+        return
+
+    def after_hid_send(self, sandbox):
+        return
+
+    def on_powersave_enable(self, sandbox):
+        return
+
+    def on_powersave_disable(self, sandbox):
+        return
+
+    def draw_badge(self, oled, mode_badge):
+        for i in self.prev_mode_badge:
+            oled.pixel(i[0] + 95, i[1], 0)
+
+        for i in mode_badge:
+            oled.pixel(i[0] + 95, i[1], 1)
+
+        self.draw_layer_name(oled)
+        oled.show()
+        self.prev_mode_badge = mode_badge
+
+    def draw_logo(self, oled, logo):
+        # self.clear_oled(oled)
+        for i in self.prev_logo:
+            oled.pixel(i[0], i[1], 0)
+        oled.show()
+
+        for i in logo:
+            oled.pixel(i[0], i[1], 1)
+        oled.show()
+        self.prev_logo = logo
+
+    def draw_key(self, oled):
+        # undraw last char
+        oled.text(self.prev_key_string, 40, 18, 0)
+
+        if len(self.key_log) < 9:
+            self.key_log.append(self.current_key)
+        else:
+            self.key_log.pop(0)
+            self.key_log.append(self.current_key)
+
+        self.prev_key_string = ""
+        # traverse in the string
+        for ele in self.key_log:
+            self.prev_key_string += ele
+
+        # draw current char
+        oled.text(self.prev_key_string, 40, 18, 1)
+        oled.show()
+        # set prev char
+        self.prev_key_log = self.key_log
+        self.prev_key = self.current_key
+
+    def draw_layer_name(self, oled):
+        # get center of display, must be int
+        center = int(self.oled_width[0] / 2)
+        # get length to middle of mode name
+        mid = int(
+            len(self.prev_layer_name) / 2 + len(self.prev_layer_name) % 2
+        )
+
+        # erase old mode name
+        oled.text(self.prev_layer_name, center - (mid * 5), 0, 0)
+
+        # get length of current mode name
+        mid = int(
+            len(self.layer_names[self.active_layers[0]]) / 2
+            + len(self.layer_names[self.active_layers[0]]) % 2
+        )
+
+        # draw current mode name
+        oled.text(
+            self.layer_names[self.active_layers[0]], center - (mid * 5), 0, 1
+        )
+
+        # set prev mode name
+        self.prev_layer_name = self.layer_names[self.active_layers[0]]
+
+
+
+    def clear_oled(self, oled):
+        # start with a blank screen
+        oled.fill(0)
+        # we just blanked the framebuffer.
+        # to push the framebuffer onto the display, we call show()
+        oled.show()
+
+
